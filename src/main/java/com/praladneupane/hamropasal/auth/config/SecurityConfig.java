e2c7126d7a4af01f78e21deea4ac5837c9fd1fd6
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-22 20:49:17 +0545

    chore: add Security Configuration

--- src/main/java/com/praladneupane/hamropasal/auth/config/SecurityConfig.java
+++ src/main/java/com/praladneupane/hamropasal/auth/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.praladneupane.hamropasal.auth.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,5 +17,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
