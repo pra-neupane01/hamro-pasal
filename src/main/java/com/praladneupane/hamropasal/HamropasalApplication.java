e94589d8f36ed955cc372bb175debcc67fa386d0
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-22 12:24:49 +0545

    initialized folder structure

---
+++ src/main/java/com/praladneupane/hamropasal/HamropasalApplication.java
@@ -0,0 +1,13 @@
+package com.praladneupane.hamropasal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamropasalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamropasalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praladneupane/hamropasal/HamropasalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praladneupane.hamropasal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamropasalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
