7c9d30e114e97cb61f80ffec0ae08107ddd5bcda
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-23 07:29:39 +0545

    chore: configure swagger

---
+++ src/main/java/com/praladneupane/hamropasal/common/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.praladneupane.hamropasal.common.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SajiloYatra API")
+                        .version("1.0")
+                        .description("API documentation for SajiloYatra Spring Boot Backend")
+                        .contact(new Contact()
+                                .name("Prakash Neupane")
+                                .email("praladneupane01@gmail.com")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(SECURITY_SCHEME_NAME)
+                )
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
